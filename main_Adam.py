5c65f2e4624c46846c21d66f95242e0ba18e5562
salvovitale <s.vitale@tudelft.nl>
2014-05-13 18:09:14 +0200

    14_05_12_sv-04ub

--- main_Adam.py
+++ main_Adam.py
@@ -10,19 +10,26 @@ axial= 'axial_nozzle/'
 sys.path.insert(0, axial)
 from matplotlib.pylab import *
 from nozzle2D import ConvDef, DivDef, ConvDiv
+from point import Point
+from bspline import Bspline
 
 
 
 if __name__ == '__main__':
 
 
-    convDef = ConvDef(l = 3.0, A = 2.0, m = 0.0, ncp = 4, type = 1)
-    divDef = DivDef(l = 8.0, A = 3.0, m = 0.0, ncp = 4, type = 1)
+    convDef = ConvDef(l = 3.0, A = 2.0, m = -0.1, ncp = 4, type = 2)
+    divDef = DivDef(l = 8.0, A = 3.0, m = 0.1, ncp = 6, type = 3)
     conv = ConvDiv(convDef)
     div = ConvDiv(divDef)
     
     conv.plot()
-    div.plot()
+#     div.plot()
+    print div.getCP()
+    
+    testP = [Point(0.0, 1.0, 0.0, 1.0), Point(0.391547869639, 1.0, 0.0, 1.0), Point(1.527864045, 2.3, 0.0, 1.0), Point(3.29771798166, 2.5, 0.0, 1.0), Point(5.527864045, 2.7527864045, 0.0, 1.0), Point(8.0, 3.0, 0.0, 1.0)]
+    div2 = Bspline(testP, p = 4)
+    div2.plot()
 
     axis('equal')
     show() 
